362c42b85ac5fb940e5f735500952fde16226b40
emmy-boy <e42425478@gmail.com>
2025-05-14 09:15:28 +0200

    Add files via upload
    and this

---
+++ contact.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>+Ticket Soft</title>
+  <link rel="icon" href="logo.png">
+  <style>
+     header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 1rem 2rem;
+      background: linear-gradient(90deg, #1e3c72, #2a5298);
+      color: white;
+    }
+    header img { height: 40px; }
+    header nav a {
+      margin: 0 15px;
+      color: white;
+      text-decoration: none;
+      font-weight: 500;
+    }
+    header nav a:hover {
+       text-decoration: none; 
+      }
+      .main{
+        height: 25rem;
+        width: 100%;
+        background-color: #f4f4f4;
+      }
+      p{
+        font-size: 1.5rem;
+        
+      }
+      .image{
+        height: 25rem;
+        width: 100%;
+      }
+      footer{
+        font-size: 1.3rem;
+        margin-left: 10rem;
+      }
+  </style>
+</head>
+<body>
+  <header>
+    <div><img src="logo.png" alt="Logo" class="logo"> <strong>+Ticket Soft</strong></div>
+    <nav>
+      <a href="index.php" id="navHome">Home</a>
+      <a href="about.php" id="navAbout">About Us</a>
+      <a href="ourservice.php" id="navServices">Services</a>
+      <a href="contact.php" id="navHelp">Help&contact</a>
+      <a href="login.php" id="navSignIn">Sign In</a>
+      <a href="signup.php" id="navSignUp">Sign Up</a>
+    </nav>
+  </header>
+  <div class="main">
+<p><strong><img src="gram.png"> Ltd | KAMPALA - KIGALI Book your ticket with us ...</strong><br>
+
+Instagram · volcano_ltd<br>
+20+ likes · -2 y<br><br>
+contact of volcano express transport from www.instagram.com<br>
++250788671703 , +250781487337 ( WhatsApp ) +256776768495 ,<br><br>
+ +256785806051. (Phone call ) +256770835989 Kampala office line for booking . @ ...
+</p>
+  </div>
+  <a href="https://www.instagram.com/volcano_ltd/">
+    <img src="vovo1.png" class="image"></a>
+   <img src="vovo2.png" class="image">
+    <img src="vovo3.png" class="image">
+     <img src="vovo4.png" class="image">
+     <img src="vovo5.png" class="image">
+     <img src="hoho1.png" class="image">
+  
+     <footer>&copy; Copy Right Ticket Soft</footer><br>
+     
+</body>
+</html>
